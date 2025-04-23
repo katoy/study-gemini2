f0614b9aa5021ce90d4a7bc8ef21bacbb0e8dbff
katoy <youichikato@gmail.com>
2025-04-24 06:01:11 +0900

    pygame_gui を削除

--- pygame_gui/main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-from app import PygameApp # app.pyからPygameAppクラスをインポート
-
-if __name__ == "__main__":
-    print("アプリケーションを開始します...")
-    # PygameAppのインスタンスを作成して実行
-    app = PygameApp()
-    app.run()
-    print("アプリケーションを終了しました。")
